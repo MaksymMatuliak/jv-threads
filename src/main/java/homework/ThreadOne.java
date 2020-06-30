82c1fecacf4520a2a9ad14424c43b82c0785b534
Максим Матуляк <maksymmatuliak@gmail.com>
2020-06-30 13:11:20 +0300

    added logs

--- src/main/java/homework/ThreadOne.java
+++ src/main/java/homework/ThreadOne.java
@@ -1,14 +1,17 @@
 package homework;
 
+import org.apache.log4j.Logger;
+
 public class ThreadOne extends Thread {
+    private static final Logger LOGGER = Logger.getLogger(ThreadOne.class);
     private int counter = 0;
 
     @Override
     public void run() {
         while (counter < 100) {
             counter++;
-            System.out.println(counter + " Thread1");
+            LOGGER.warn(counter + " Thread1");
         }
-        System.out.println("First tread finished race!");
+        LOGGER.warn("First tread finished race!");
     }
 }
--- src/main/java/homework/ThreadTwo.java
+++ src/main/java/homework/ThreadTwo.java
@@ -1,14 +1,17 @@
 package homework;
 
+import org.apache.log4j.Logger;
+
 public class ThreadTwo implements Runnable {
+    private static final Logger LOGGER = Logger.getLogger(ThreadTwo.class);
     private int counter = 0;
 
     @Override
     public void run() {
         while (counter < 100) {
             counter++;
-            System.out.println(counter + " Thread2");
+            LOGGER.warn(counter + " Thread2");
         }
-        System.out.println("Second tread finished race!");
+        LOGGER.warn("Second tread finished race!");
     }
 }
