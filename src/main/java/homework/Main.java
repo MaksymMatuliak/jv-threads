38aa76d410106d00521f2dd2e6d45a61ebe9c578
Максим Матуляк <maksymmatuliak@gmail.com>
2020-06-30 14:57:58 +0300

    added new logs and fixed naming

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -3,8 +3,8 @@ package homework;
 public class Main {
     public static void main(String[] args) {
         Race race = new Race();
-        ThreadOne threadOne = new ThreadOne(race);
-        ThreadTwo runnable = new ThreadTwo(race);
+        ThreadClass threadOne = new ThreadClass(race);
+        RunnableClass runnable = new RunnableClass(race);
         Thread threadTwo = new Thread(runnable);
         threadOne.start();
         threadTwo.start();
